71a7df8b9c02f070479ad4840830a662dc05746d
leandroarndt <poetalirico@gmail.com>
2026-07-26 00:34:54 -0300

    Full installation script

--- install.bat
+++ install.bat
@@ -1,19 +1,37 @@
-echo OFF
-echo "Creating virtual environment..."
-py -m venv .env
+@ECHO OFF
 
-echo "Activating virtual environment..."
+SET "ARG1=%1"
+REM SET "PYTHON=PY"
+
+IF "%ARG1%"=="" (
+    set "PYTHON=py"
+)
+IF "%ARG1%"=="-h" (
+    ECHO Usage: install.bat [--help]
+    ECHO Installs the application and its dependencies.
+    ECHO.
+    ECHO Options:
+    ECHO   --help      Show this help message and exit.
+    EXIT /B 0
+)
+IF "%ARG1%"=="-p" (
+    SET "PYTHON=%~2"
+)
+
+ECHO Creating virtual environment...
+%PYTHON% -m venv .env
+
+ECHO Activating virtual environment...
 call .env\Scripts\activate.bat
 
-echo "Installing required Python modules..."
+ECHO Installing required Python modules...
 pip install -r requirements.txt
 
-echo "Compiling translations..."
+ECHO Compiling translations...
 pybabel compile -D utilpaisagem -d resources/locale
 
-echo "Creating shortcut..."
-python shortcut.py
+ECHO Creating shortcut...
+%~dp0\.env\Scripts\python.exe %~dp0\shortcut.py
 
-echo ""
-echo "Installation successful."
-echo "Run Útil paisagem using the application shortcut."
+ECHO.
+ECHO Run Util paisagem using the application shortcut.
