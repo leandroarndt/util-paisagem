c58016ad5c55cc94d921ed89ea4dd464637194b6
leandroarndt <poetalirico@gmail.com>
2026-07-30 10:28:15 -0300

    Attribute error on print

--- utilpaisagem/scenery/tile.py
+++ utilpaisagem/scenery/tile.py
@@ -236,11 +236,11 @@ class Tile(object):
                 if not Path(path / f"{self.index}.{log['INFO']['format']}").exists():
                     p = Path(path / f"{self.index}.{log['INFO']['format']}")
                     if self.upstream_queue is None:
-                        print('Previous download was not found where expected ("{p}"). Downloading again.').format(p=p)
+                        print('Previous download was not found where expected ("{p}"). Downloading again.'.format(p=p))
                     else:
                         self.upstream_queue.put_nowait(format_status(
                             _('Previous download was not found where expected ("{p}"). Downloading again.').format(p=p),
-                            self
+                            self    
                         ))
                     raise AssertionError
                 if self.upstream_queue is None:
