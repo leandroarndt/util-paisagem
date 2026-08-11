fb8283ed5d8b7bfb111e1929b21de3177fa8aad8
leandroarndt <poetalirico@gmail.com>
2026-08-11 19:19:09 -0300

    Remove print

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 base_path = Path(__file__).parent
 resources_path = base_path / 'resources'
-print(base_path)
 
 translation = gettext.translation('utilpaisagem', resources_path / 'locale', fallback=True, languages=[locale.getlocale()[0] or 'en_US'])
 translation.install()
